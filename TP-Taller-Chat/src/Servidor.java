1eddeb3d139413a08802c74723a4092c9fb2aeee
Nico <pvazsquezpetracca@alumno.unlam.edu.ar>
2021-11-01 22:21:34 -0300

    Probando...

---
+++ TP-Taller-Chat/src/Servidor.java
@@ -0,0 +1,35 @@
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Servidor {
+	public Servidor(int puerto) throws IOException {
+		ServerSocket servidor = new ServerSocket(puerto);
+		
+		System.out.println("Server inicializando...");
+		
+		for(int i = 1; i <= 3; i++) {
+			Socket socket = servidor.accept();
+			DataOutputStream salida = new DataOutputStream(socket.getOutputStream());
+			salida.writeUTF("Sos el cliente Nro: " + i);
+			
+			salida.close();
+			socket.close();
+		}
+		
+		System.out.println("Server Finalizado");
+		servidor.close();
+	}
+	
+	public static void main(String[] args) {
+		
+		try {
+			new Servidor(20000);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
